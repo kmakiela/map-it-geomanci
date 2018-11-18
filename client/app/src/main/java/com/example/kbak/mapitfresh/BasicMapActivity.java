68d85e5d3d2f5538bab0bd40c13a10df4c54c064
Karol Bąk <bak.karolek@gmail.com>
2018-11-18 12:00:39 +0100

    uff. Seems to work

--- client/app/src/main/java/com/example/kbak/mapitfresh/BasicMapActivity.java
+++ client/app/src/main/java/com/example/kbak/mapitfresh/BasicMapActivity.java
@@ -1,13 +1,22 @@
 package com.example.kbak.mapitfresh;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
 import com.here.android.mpa.common.GeoCoordinate;
+import com.here.android.mpa.common.Image;
 import com.here.android.mpa.common.OnEngineInitListener;
 import com.here.android.mpa.common.ViewObject;
 import com.here.android.mpa.mapping.Map;
@@ -15,12 +24,19 @@ import com.here.android.mpa.mapping.MapFragment;
 import com.here.android.mpa.mapping.MapGesture;
 import com.here.android.mpa.mapping.MapMarker;
 import com.here.android.mpa.mapping.MapObject;
-
-import org.w3c.dom.Text;
+import com.here.android.mpa.mapping.MapRoute;
+import com.here.android.mpa.routing.RouteManager;
+import com.here.android.mpa.routing.RoutePlan;
+import com.here.android.mpa.routing.RouteResult;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.TextHttpResponseHandler;
 
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
 
 public class BasicMapActivity extends AppCompatActivity {
 
@@ -30,10 +46,15 @@ public class BasicMapActivity extends AppCompatActivity {
     // map fragment embedded in this activity
     private MapFragment mapFragment = null;
     private TextView addressField = null;
+    private TextView countryField = null;
     private Button okButton = null;
+    private Button sendButton = null;
 
     private List<GeoCoordinate> wayPoints = new LinkedList<>();
-    private ActualPosition actualPosition = new ActualPosition();
+    private ActualPosition actualPosition;
+
+    RouteManager rm = new RouteManager();
+    RoutePlan routePlan = new RoutePlan();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -47,22 +68,21 @@ public class BasicMapActivity extends AppCompatActivity {
         // Search for the map fragment to finish setup by calling init().
         mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapfragment);
         addressField = (TextView) findViewById(R.id.address);
+        countryField = (TextView) findViewById(R.id.country);
         okButton = (Button) findViewById(R.id.ok_button);
+        sendButton = (Button) findViewById(R.id.send_button);
+        sendButton.setEnabled(false);
+
+        actualPosition = new ActualPosition();
 
         mapFragment.init(new OnEngineInitListener() {
             @Override
             public void onEngineInitializationCompleted(OnEngineInitListener.Error error) {
                 if (error == OnEngineInitListener.Error.NONE) {
-                    // retrieve a reference of the map from the map fragment
                     map = mapFragment.getMap();
-                    // Set the map center to the Vancouver region (no animation)
-                    map.setCenter(new GeoCoordinate(49.196261, -123.004773, 0.0),
-                            Map.Animation.NONE);
-                    // Set the zoom level to the average between min and max
                     map.setZoomLevel((map.getMaxZoomLevel() + map.getMinZoomLevel()) / 2);
-                    mapFragment.getMapGesture().addOnGestureListener(new MapOnGestureListener(map, addressField, actualPosition));
-
-
+                    map.setCenter(new GeoCoordinate(50.099433, 19.9955930, 0), Map.Animation.NONE);
+                    mapFragment.getMapGesture().addOnGestureListener(new MapOnGestureListener(map, addressField, countryField, actualPosition));
                 } else {
                     System.out.println("ERROR: Cannot initialize Map Fragment");
                 }
@@ -73,6 +93,47 @@ public class BasicMapActivity extends AppCompatActivity {
         okButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 wayPoints.add(actualPosition.position);
+                routePlan.addWaypoint(actualPosition.position);
+                if (wayPoints.size() >= 10)
+                    sendButton.setEnabled(true);
+            }
+        });
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                class RouteListener implements RouteManager.Listener {
+                    public void onProgress(int percentage) {
+                    }
+
+                    public void onCalculateRouteFinished(RouteManager.Error error, List<RouteResult> routeResult) {
+                        if (error == RouteManager.Error.NONE) {
+                            MapRoute mapRoute = new MapRoute(routeResult.get(0).getRoute());
+                            map.addMapObject(mapRoute);
+                        } else {
+                        }
+                    }
+                }
+
+                RequestParams rp = new RequestParams();
+                Gson gson = new Gson();
+                rp.add("waypoints", gson.toJson(wayPoints));
+
+                RESTprovider.get("/?query=", rp, new TextHttpResponseHandler() {
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                        Log.d("postObjectJSON", throwable.toString());
+                    }
+
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, String response) {
+                            Log.d("postObjectJSON", response.toString());
+                            sendButton.setText("Success!");
+                            
+                    }
+                });
+
+                rm.calculateRoute(routePlan, new RouteListener());
+
             }
         });
     }
--- client/app/src/main/java/com/example/kbak/mapitfresh/MapOnGestureListener.java
+++ client/app/src/main/java/com/example/kbak/mapitfresh/MapOnGestureListener.java
@@ -9,21 +9,26 @@ import com.here.android.mpa.common.GeoCoordinate;
 import com.here.android.mpa.common.ViewObject;
 import com.here.android.mpa.mapping.MapGesture;
 import com.here.android.mpa.mapping.Map;
+import com.here.android.mpa.mapping.MapMarker;
 import com.here.android.mpa.search.Address;
 import com.here.android.mpa.search.ErrorCode;
 import com.here.android.mpa.search.ResultListener;
 import com.here.android.mpa.search.ReverseGeocodeRequest;
 
+import java.io.IOException;
 import java.util.List;
 
 public class MapOnGestureListener implements GestureOverlayView.OnGestureListener, MapGesture.OnGestureListener {
     private Map map;
     private TextView addressField;
+    private TextView countryField;
     private ActualPosition actualPosition;
 
-    MapOnGestureListener(Map map, TextView addressField, ActualPosition actualPosition) {
+
+    MapOnGestureListener(Map map, TextView addressField, TextView countryField, ActualPosition actualPosition) {
         this.map = map;
         this.addressField = addressField;
+        this.countryField = countryField;
         this.actualPosition = actualPosition;
     }
 
@@ -55,10 +60,9 @@ public class MapOnGestureListener implements GestureOverlayView.OnGestureListene
         map.setCenter(new GeoCoordinate(position),
                 Map.Animation.LINEAR);
         map.setZoomLevel(level);
-        ResultListener<Address> listener = new TapGeocodeListener(addressField);
+        ResultListener<Address> listener = new TapGeocodeListener(addressField, countryField);
         ReverseGeocodeRequest request = new ReverseGeocodeRequest(position);
         actualPosition.position = position;
-
         if (request.execute(listener) != ErrorCode.NONE){
             addressField.setText("Invalid starting point");
         }
@@ -104,10 +108,6 @@ public class MapOnGestureListener implements GestureOverlayView.OnGestureListene
 
     @Override
     public boolean onTwoFingerTapEvent(PointF p) {
-        double level = map.getMinZoomLevel() + map.getMaxZoomLevel() / 2;
-        map.setCenter(new GeoCoordinate(49.196261, -123.004773),
-                Map.Animation.NONE);
-        map.setZoomLevel(level);
         return true;
     }
 
